714a3141a407bca35a3c85cdd15373a1a3686d4b
DavidHanover <DavidHanover89@gmail.com>
2019-10-04 14:51:47 -0700

    created more styled components, styledtetris and began styledstage component

--- src/Components/Tetris.js
+++ src/Components/Tetris.js
@@ -1,6 +1,7 @@
 import Stage from "./Stage";
 import Display from "./Display";
 import StartButton from "./StartButton";
+import { StyledTetris, StyledTetrisWrapper } from "./styles/StyledTetris";
 
 import React from "react";
 
@@ -8,17 +9,19 @@ import { createStage } from "../gameHelpers";
 
 const Tetris = () => {
   return (
-    <div>
-      <Stage stage={createStage()} />
-      <aside>
-        <div>
-          <Display text="Score" />
-          <Display text="Rows" />
-          <Display text="Level" />
-        </div>
-        <StartButton />
-      </aside>
-    </div>
+    <StyledTetrisWrapper>
+      <StyledTetris>
+        <Stage stage={createStage()} />
+        <aside>
+          <div>
+            <Display text="Score" />
+            <Display text="Rows" />
+            <Display text="Level" />
+          </div>
+          <StartButton />
+        </aside>
+      </StyledTetris>
+    </StyledTetrisWrapper>
   );
 };
 
