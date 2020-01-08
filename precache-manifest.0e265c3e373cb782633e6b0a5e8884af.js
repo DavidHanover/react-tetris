ba14694aa8230ccd666afea331cdf862df507124
DavidHanover <DavidHanover89@gmail.com>
2020-01-08 00:06:58 -0800

    Updates

---
+++ precache-manifest.0e265c3e373cb782633e6b0a5e8884af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3180e82f6d60b15199371470816d9c0",
+    "url": "/react-tetris/build/index.html"
+  },
+  {
+    "revision": "f803286bd451b05aad09",
+    "url": "/react-tetris/build/static/css/main.50e152d8.chunk.css"
+  },
+  {
+    "revision": "a68c24ff485dbb217fc5",
+    "url": "/react-tetris/build/static/js/2.b6b43296.chunk.js"
+  },
+  {
+    "revision": "f803286bd451b05aad09",
+    "url": "/react-tetris/build/static/js/main.fa09c096.chunk.js"
+  },
+  {
+    "revision": "1286892a411cf13aabfc",
+    "url": "/react-tetris/build/static/js/runtime~main.b59a6921.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/react-tetris/build/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "2810fe6b5435e75b3dad63e341b97d02",
+    "url": "/react-tetris/build/static/media/bg.2810fe6b.png"
+  }
+]);
