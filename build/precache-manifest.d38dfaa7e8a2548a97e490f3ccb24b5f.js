ceceb58684f5df4f1691f011ed7789fd3f150f16
DavidHanover <DavidHanover89@gmail.com>
2020-01-07 23:41:07 -0800

    committing build folder for deployment

---
+++ build/precache-manifest.d38dfaa7e8a2548a97e490f3ccb24b5f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77b8359d5cedddb7ca5ace738ebbe8f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75c967aa49117e6cdb0f",
+    "url": "/static/css/main.d9ff8bbe.chunk.css"
+  },
+  {
+    "revision": "6da3d5b2a673c4d1fd9c",
+    "url": "/static/js/2.6d895a3d.chunk.js"
+  },
+  {
+    "revision": "75c967aa49117e6cdb0f",
+    "url": "/static/js/main.fa09c096.chunk.js"
+  },
+  {
+    "revision": "dfe5b6acd2bf313a622d",
+    "url": "/static/js/runtime~main.8d4fe0f9.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "2810fe6b5435e75b3dad63e341b97d02",
+    "url": "/static/media/bg.2810fe6b.png"
+  }
+]);
